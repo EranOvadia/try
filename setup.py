22b57ed588de4d16d738b72404b14d00bf3c3c6f
EranOvadia <43248536+EranOvadia@users.noreply.github.com>
2019-12-17 12:40:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-EranOvadia",  # Replace with your own username
+    name="HelloTry",  # Replace with your own username
     version="1.0",
     author="Eran Ovadia",
     author_email="eran.ovadia8@gmail.com",
